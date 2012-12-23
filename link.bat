a1fad847b3c1ee6082323571f7813bee3df4200c
Urasaki Yuki <ursk4git@live.jp>
2012-12-24 00:04:47 +0900

    add link (.vim => vim)

--- link.bat
+++ link.bat
@@ -1,6 +1,6 @@
 @rem clean
 del %HOME%\_vimrc %HOME%\_gvimrc
-@rem delete %HOME%\vim
+rmdir %HOME%\.vim
 
 @set DF_DIR=%HOME%\dotfiles
 
@@ -8,5 +8,6 @@ del %HOME%\_vimrc %HOME%\_gvimrc
 mklink %HOME%\_vimrc %DF_DIR%\vim\.vimrc
 mklink %HOME%\_gvimrc %DF_DIR%\vim\.gvimrc
 
+mklink /D %HOME%\.vim %DF_DIR%\vim
 pause
 
