a78a7cc9a22aa67b688e689de9305788207e7cad
urasa <ursk4git@live.jp>
2013-01-21 19:40:20 +0900

    for gvim

--- link.sh
+++ link.sh
@@ -2,7 +2,9 @@
 
 cd ~
 mv .vimrc .vimrc.bak
+mv .gvimrc .gvimrc.bak
 rm .vim
 ln -v --symbolic dotfiles/vim/.vimrc .vimrc
+ln -v --symbolic dotfiles/vim/.gvimrc .gvimrc
 ln -v --symbolic --directory dotfiles/vim .vim
 
