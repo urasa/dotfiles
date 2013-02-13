59abec77331e2a05a02344354239a4abc15a6790
urasa <ursk4git@live.jp>
2013-02-14 05:29:13 +0900

    add git config

--- link.sh
+++ link.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 cd ~
+## vim
 [ -e .vimrc  ] && mv .vimrc .vimrc.bak
 [ -e .gvimrc ] && mv .gvimrc .gvimrc.bak
 [ -e .vim    ] && rm .vim
 ln -v -s dotfiles/vim/.vimrc .vimrc
 ln -v -s dotfiles/vim/.gvimrc .gvimrc
 ln -v -s dotfiles/vim .vim
+
+## git
+[ -e .gitconfig     ] && mv .gitconfig .gitconfig.bak
+[ -e .global_ignore ] && mv .global_ignore .global_ignore.bak
+ln -v -s dotfiles/git/gitconfig .gitconfig
+ln -v -s dotfiles/git/global_ignore .global_ignore
 
